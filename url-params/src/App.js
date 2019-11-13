e57ceb1666106b2c62f4cba05f5e740a6d719aea
Michael Giebner <mikegiebner@gmail.com>
2019-11-12 19:44:11 -0500

    Chapter 155 complete

--- url-params/src/App.js
+++ url-params/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Route} from "react-router-dom"
 import Food from "./Food"
+import Meal from "./Meal"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/food/:name" render={(routeProps) => <Food {...routeProps} /> } />
+      <Route exact path="/food/:foodName/drink/:drinkName" component={Meal} />
     </div>
   );
 }
