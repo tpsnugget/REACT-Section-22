06c7f722957ef4e96c321fd27d3f4ca4f4fc8660
Michael Giebner <mikegiebner@gmail.com>
2019-11-13 11:01:27 -0500

    Chapter 157 complete

--- url-params/src/App.js
+++ url-params/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import {Route} from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 import Food from "./Food"
 import Meal from "./Meal"
+import FoodSearch from "./FoodSearch"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/food/:name" render={(routeProps) => <Food {...routeProps} /> } />
-      <Route exact path="/food/:foodName/drink/:drinkName" component={Meal} />
+      <Switch>
+        <Route exact path="/food/:name" render={(routeProps) => <Food {...routeProps} />} />
+        <Route exact path="/food/:foodName/drink/:drinkName" component={Meal} />
+        <Route exact path="/" render={() => <FoodSearch />} />
+        <Route render={() => <h1>404 Error Not Found</h1>} />
+      </Switch>
     </div>
   );
 }
